5f9cee4c1a9bc16ed2eba5e1b096688ef3f22427
Natalia Dominique <carcamosalinasnatalia@gmail.com>
2023-09-05 16:37:35 -0300

    commit bacan

---
+++ js/credenciales.js
@@ -0,0 +1,5 @@
+// TODO(developer): Set to client ID and API key from the Developer Console
+const CLIENT_ID =
+    "856864012106-5bhv5v9b32vufhtgr1it2d8kdvkatr29.apps.googleusercontent.com";
+const API_KEY = "AIzaSyCoGL3ozh3o80JJ7zIHFGgIJ5YWE2uu6-o";
+const SPREADSHEET = "1TOjC5q8OgGo40vfi9zVc6ZySamEalAQxPKpW3RfKmFw";
